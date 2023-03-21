67b3fded4c0080c4eaba3694dd9bad9d092ff590
njcinnamond <njcinnamond@gmail.com>
2023-03-21 17:16:25 -0400

    remove crypto-browserify cfg

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,6 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = function override(config) {
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, {
-        "crypto": require.resolve("crypto-browserify"),
         "stream": require.resolve("stream-browserify"),
         "assert": require.resolve("assert"),
         "http": require.resolve("stream-http"),
