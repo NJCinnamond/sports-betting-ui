3e1e3e43e3e9e50cb995ba21906ab19cda3c36ab
njcinnamond <njcinnamond@gmail.com>
2023-03-21 17:36:55 -0400

    revert module cfg chg

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,10 @@
 const webpack = require('webpack');
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = function override(config) {
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
             process: 'process/browser',
             Buffer: ['buffer', 'Buffer']
-        }),
-        new NodePolyfillPlugin({
-            excludeAliases: ['console-browserify'],
         }),
     ])
     return config;
